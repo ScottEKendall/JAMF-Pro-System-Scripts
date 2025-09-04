58d4a736102dcde1a6ba09b642b8366867c008a0
Scott Kendall <106453475+ScottEKendall@users.noreply.github.com>
2025-09-04 08:42:38 -0400

    System Scripts that I use for our JAMF server

--- Maintenance - Passwords - Populate Plist File (inTune).sh
+++ Maintenance - Passwords - Populate Plist File (inTune).sh
@@ -2,13 +2,12 @@
 #
 # by: Scott Kendall
 #
-# Written: 04/17/2025
-# Last updated: 08/14/2025
+# Written: 09/03/2025
+# Last updated: 09/04/2025
 
 # Script to populate /Library/Managed Preferences/com.gianteagle.jss file with uses EntraID password info
 # 
 # 1.0 - Initial code
-# 1.1 - Add logic to write out PasswordAge as well to the plist file
 #
 ######################################################################################################
 #
@@ -17,217 +16,64 @@
 ######################################################################################################
 
 LOGGED_IN_USER=$( scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/ && ! /loginwindow/ { print $3 }' )
+MS_USER_NAME=$(dscl . read /Users/$LOGGED_IN_USER | grep "NetworkUser" | awk -F ':' '{print $2}' | xargs)
 
 SUPPORT_DIR="/Library/Application Support/GiantEagle"
-LOG_STAMP=$(echo $(/bin/date +%Y%m%d))
-LOG_DIR="${SUPPORT_DIR}/logs"
-LOG_FILE="${LOG_DIR}/ViewDeviceInventory.log"
+LOG_FILE="${SUPPORT_DIR}/logs/PopulatePasswordPlist"
 JQ_INSTALL_POLICY="install_jq"
-MAC_SERIAL_NUMBER=$(scutil --get HostName)
 JSS_FILE="/Library/Managed Preferences/com.gianteagle.jss.plist"
 
-###################################################
+##################################################
 #
-# App Specfic variables (Feel free to change these)
-#
-###################################################
+# Passed in variables
+# 
+#################################################
 
 JAMF_LOGGED_IN_USER=$3                          # Passed in by JAMF automatically  
 CLIENT_ID="$4"
 CLIENT_SECRET="$5"
+TENANT_ID="$6"
+
 ####################################################################################################
 #
 # Functions
 #
 ####################################################################################################
 
-function create_log_directory ()
-{
-    # Ensure that the log directory and the log files exist. If they
-    # do not then create them and set the permissions.
-    #
-    # RETURN: None
-
-	# If the log directory doesnt exist - create it and set the permissions
-	[[ ! -d "${LOG_DIR}" ]] && /bin/mkdir -p "${LOG_DIR}"
-	/bin/chmod 755 "${LOG_DIR}"
-
-	# If the log file does not exist - create it and set the permissions
-	[[ ! -f "${LOG_FILE}" ]] && /usr/bin/touch "${LOG_FILE}"
-	/bin/chmod 644 "${LOG_FILE}"
-}
-
-function logMe () 
-{
-    # Basic two pronged logging function that will log like this:
-    #
-    # 20231204 12:00:00: Some message here
-    #
-    # This function logs both to STDOUT/STDERR and a file
-    # The log file is set by the $LOG_FILE variable.
-    #
-    # RETURN: None
-    echo "$(/bin/date '+%Y-%m-%d %H:%M:%S'): ${1}" | tee -a "${LOG_FILE}"
-}
-
 function check_support_files ()
 {
     [[ $(which jq) == *"not found"* ]] && /usr/local/bin/jamf policy -trigger ${JQ_INSTALL_POLICY}
-}
-
-function cleanup_and_exit ()
-{
-	[[ -f ${JSON_OPTIONS} ]] && /bin/rm -rf ${JSON_OPTIONS}
-	[[ -f ${TMP_FILE_STORAGE} ]] && /bin/rm -rf ${TMP_FILE_STORAGE}
-    [[ -f ${DIALOG_COMMAND_FILE} ]] && /bin/rm -rf ${DIALOG_COMMAND_FILE}
-	exit 0
-}
-
-function check_JSS_Connection()
-{
-    # PURPOSE: Function to check connectivity to the Jamf Pro server
-    # RETURN: None
-    # EXPECTED: None
-
-    if ! /usr/local/bin/jamf -checkjssconnection -retry 5; then
-        logMe "Error: JSS connection not active."
-        exit 1
-    fi
-    logMe "JSS connection active!"
-}
-
-function get_JAMF_Server ()
-{
-    # PURPOSE: Retreive your JAMF server URL from the preferences file
-    # RETURN: None
-    # EXPECTED: None
-
-    jamfpro_url=$(/usr/bin/defaults read /Library/Preferences/com.jamfsoftware.jamf.plist jss_url)
-    logMe "JAMF Pro server is: $jamfpro_url"
-}
-
-function get_JamfPro_Classic_API_Token ()
-{
-    # PURPOSE: Get a new bearer token for API authentication.  This is used if you are using a JAMF Pro ID & password to obtain the API (Bearer token)
-    # PARMS: None
-    # RETURN: api_token
-    # EXPECTED: CLIENT_ID, CLIENT_SECRET, jamfpro_url
-
-     api_token=$(/usr/bin/curl -X POST --silent -u "${CLIENT_ID}:${CLIENT_SECRET}" "${jamfpro_url}/api/v1/auth/token" | plutil -extract token raw -)
-
-}
-
-function get_JAMF_Access_Token()
-{
-    # PURPOSE: obtain an OAuth bearer token for API authentication.  This is used if you are using  Client ID & Secret credentials)
-    # RETURN: connection stringe (either error code or valid data)
-    # PARMS: None
-    # EXPECTED: CLIENT_ID, CLIENT_SECRET, jamfpro_url
-
-    returnval=$(curl --silent --location --request POST "${jamfpro_url}/api/oauth/token" \
-        --header "Content-Type: application/x-www-form-urlencoded" \
-        --data-urlencode "client_id=${CLIENT_ID}" \
-        --data-urlencode "grant_type=client_credentials" \
-        --data-urlencode "client_secret=${CLIENT_SECRET}")
-    
-    if [[ -z "$returnval" ]]; then
-        logMe "Check Jamf URL"
-        exit 1
-    elif [[ "$returnval" == '{"error":"invalid_client"}' ]]; then
-        logMe "Check the API Client credentials and permissions"
-        exit 1
-    fi
-    
-    api_token=$(echo "$returnval" | plutil -extract access_token raw -)
-}
-
-function get_JAMF_DeviceID ()
-{
-    # PURPOSE: uses the serial number or hostname to get the device ID (UDID) from the JAMF Pro server. (JAMF pro 11.5.1 or higher)
-    # RETURN: the device ID (UDID) for the device in question.
-    # PARMS: $1 - search identifier to use (Serial or Hostname)
-
-    [[ "$1" == "Hostname" ]] && type="general.name" || type="hardware.serialNumber"
-
-    jamfID=$(/usr/bin/curl --silent --fail -H "Authorization: Bearer ${api_token}" -H "Accept: application/json" "${jamfpro_url}/api/v1/computers-inventory?filter=${type}==${computer_id}" | /usr/bin/plutil -extract results.0.id raw -)
-
-    # if ID is not found, display a message or something...
-    if [[ "$jamfID" == *"Could not extract value"* || "$jamfID" == *"null"* ]]; then
-    	logMe "Error: Could not find inventory record for JAMF Device #${computer_id}"
-        logMe "Last error message: $jamfID"
-        exit 1
-    fi
-    echo $jamfID
-}
-
-function invalidate_JAMF_Token()
-{
-    # PURPOSE: invalidate the JAMF Token to the server
-    # RETURN: None
-    # Expected jamfpro_url, ap_token
-
-    returnval=$(/usr/bin/curl -w "%{http_code}" -H "Authorization: Bearer ${api_token}" "${jamfpro_url}/api/v1/auth/invalidate-token" -X POST -s -o /dev/null)
-
-    if [[ $returnval == 204 ]]; then
-        logMe "Token successfully invalidated"
-    elif [[ $returnval == 401 ]]; then
-        logMe "Token already invalid"
-    else
-        logMe "Unexpected response code: $returnval"
-        exit 1  # Or handle it in a different way (e.g., retry or log the error)
-    fi    
-}
-
-function get_JAMF_InventoryRecord ()
-{
-    # PURPOSE: Uses the JAMF 
-    # RETURN: the device ID (UDID) for the device in question.
-    # PARMS: $1 - Section of inventory record to retrieve (GENERAL, DISK_ENCRYPTION, PURCHASING, APPLICATIONS, STORAGE, USER_AND_LOCATION, CONFIGURATION_PROFILES, PRINTERS, 
-    #                                                      SERVICES, HARDWARE, LOCAL_USER_ACCOUNTS, CERTIFICATES, ATTACHMENTS, PLUGINS, PACKAGE_RECEIPTS, FONTS, SECURITY, OPERATING_SYSTEM,
-    #                                                      LICENSED_SOFTWARE, IBEACONS, SOFTWARE_UPDATES, EXTENSION_ATTRIBUTES, CONTENT_CACHING, GROUP_MEMBERSHIPS)
-    retval=$(/usr/bin/curl --silent --fail  -H "Authorization: Bearer ${api_token}" -H "Accept: application/json" "${jamfpro_url}api/v1/computers-inventory/$jamfID?section=$1") # 2>/dev/null)
-    echo $retval | tr -d '\n'
 }
 
 function get_MS_access_token ()
 {
     # PURPOSE: obtain the MS inTune Graph API Token
     # RETURN: access_token
-    # Expected ms_url
+    # EXPECTED: TENANT_ID, CLIENT_ID, CLIENT_SECRET
 
-    token_response=$(curl -s -X POST "https://login.microsoftonline.com/$tenant_id/oauth2/v2.0/token" \
-    -H "Content-Type: application/x-www-form-urlencoded" -d "grant_type=client_credentials&client_id=$client_id&client_secret=$client_secret&scope=https://graph.microsoft.com/.default")
+    token_response=$(curl -s -X POST "https://login.microsoftonline.com/$TENANT_ID/oauth2/v2.0/token" \
+    -H "Content-Type: application/x-www-form-urlencoded" -d "grant_type=client_credentials&client_id=$CLIENT_ID&client_secret=$CLIENT_SECRET&scope=https://graph.microsoft.com/.default")
 
     ms_access_token=$(echo "$token_response" | jq -r '.access_token')
 
-    if [[ "$access_token" == "null" ]] || [[ -z "$access_token" ]]; then
+    if [[ "$ms_access_token" == "null" ]] || [[ -z "$ms_access_token" ]]; then
         echo "Failed to acquire access token"
         echo "$token_response"
         exit 1
     fi
-
-    echo "Access token acquired"
+    echo "Valid Token Acquired"
 }
 
 function get_ms_user_data ()
 {
     # PURPOSE: Retrieve the user's Graph API Record
     # RETURN: last_password_change
-    # Expected ms_url
+    # EXPECTED: MS_USER_NAME, ms_access_token
 
-    user_response=$(curl -s -X GET "https://graph.microsoft.com/v1.0/users/$microsoftUserName?\$select=lastPasswordChangeDateTime" \
-    -H "Authorization: Bearer $ms_access_token")
+    user_response=$(curl -s -X GET "https://graph.microsoft.com/v1.0/users/$MS_USER_NAME?\$select=lastPasswordChangeDateTime" -H "Authorization: Bearer $ms_access_token")
 
     last_password_change=$(echo "$user_response" | jq -r '.lastPasswordChangeDateTime')
-
-    if [[ "$last_password_change" == "null" ]] || [[ -z "$last_password_change" ]]; then
-        echo "Could not retrieve lastPasswordChangeDateTime"
-        echo "$user_response"
-        exit 1
-    else
-        #echo "User: $user_principal_name"
-        echo $last_password_change
-    fi
+    echo $last_password_change
 
 }
 
@@ -255,54 +101,40 @@ function calculate_days_between() {
 # Main Script
 #
 ####################################################################################################
-declare jamfpro_url
-declare api_token
-declare jamfID
-declare search_type
-declare recordGeneral
-declare recordExtensions
+declare ms_access_token
+declare last_password_change
 
-search_type="Hostname"
-computer_id=$MAC_SERIAL_NUMBER
-microsoftUserName=$(dscl . read /Users/$LOGGED_IN_USER dsAttrTypeStandard:AltSecurityIdentities | awk -F'SSO:' '/PlatformSSO/ {print $2}')
+forceRecon="No"
 
-autoload 'is-at-least'
-
-create_log_directory
 check_support_files
 
-# Perform JAMF API calls to locate device & retrieve device info
+# Routine for getitng the info from MS Intune Graph API
+get_MS_access_token
+newPasswordDate=$(get_ms_user_data)
 
-get_JAMF_Server
-get_JamfPro_Classic_API_Token
-jamfID=$(get_JAMF_DeviceID ${search_type})
-
-recordExtensions=$(get_JAMF_InventoryRecord "EXTENSION_ATTRIBUTES")
-#logMe "INFO: Inventory Record Info: $recordExtensions"
-forceRecon="No"
-# These variables are specific to JAMF EA fields
-newPasswordDate=$(echo $recordExtensions | jq -r '.extensionAttributes[] | select(.name == "Password Change Date") | .values[]' )
-passwordAge=$(echo $recordExtensions | jq -r '.extensionAttributes[] | select(.name == "Password Age") | .values[]' )
-logMe "INFO: inTune password date shows: $newPasswordDate"
-logMe "INFO: Curent Password Age: $passwordAge"
-
-if [[ -z $newPasswordDate ]]; then
+# the date of 1601-01-01T00:00:00Z means that a user has never changed their password.  That is the default MS Epoch time...
+if [[ -z $newPasswordDate ]] || [[ "$newPasswordDate" == "null" ]] || "$newPasswordDate" == "1601-01-01T00:00:00Z"; then
 	# Couldn't find the key in the plist file, so we have to rely on the local login password last changed date
-    curUser=$( scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/ && ! /loginwindow/ { print $3 }' )
-    passwordAge=$(expr $(expr $(date +%s) - $(dscl . read /Users/${curUser} | grep -A1 passwordLastSetTime | grep real | awk -F'real>|</real' '{print $2}' | awk -F'.' '{print $1}')) / 86400)
+    
+    passwordAge=$(expr $(expr $(date +%s) - $(dscl . read /Users/${LOGGED_IN_USER} | grep -A1 passwordLastSetTime | grep real | awk -F'real>|</real' '{print $2}' | awk -F'.' '{print $1}')) / 86400)
+    
 	[[ -z ${passwordAge} ]] && passwordAge=0
 	newPasswordDate=$(date -j -v-${passwordAge}d +"%Y-%m-%dT12:00:00Z")
     forceRecon="Yes"
     echo "INFO: The PLIST entry is blank.  New Password Date is: ${newPasswordDate} based off of local system password."
     echo "INFO: JAMF inventory update triggered"
-else
-    passwordAge=$(calculate_days_between "$newPasswordDate")
 fi
+# Determine the password age 
+passwordAge=$(calculate_days_between "$newPasswordDate")
 
+echo "INFO: inTune password date shows: $newPasswordDate"
+echo "INFO: Curent Password Age: $passwordAge"
+
+# Get the value of the date stored in our plist file
 retval=$(/usr/libexec/plistbuddy -c "print PasswordLastChanged" $JSS_FILE 2>&1)
 
 # If the password is blank, then set it to the calculated value
-[[ -z $retval ]] && retval=$newPasswordDate
+[[ -z $retval ]]  || [[ "$retval" == "null" ]] && retval=$newPasswordDate
 
 # do a quick santity check...convert both dates to epoch time
 timestamp_lastretval=$(date -j -f "%Y-%m-%dT%H:%M:%SZ" $retval +%s)
@@ -310,22 +142,22 @@ timestamp_lastPass=$(date -j -f "%Y-%m-%dT%H:%M:%SZ" $newPasswordDate +%s)
 
 # and see if the inTune password is greater than the recorded date, if so, then set it to the inTune password
 if [[ $timestamp_lastPass -gt $timestamp_lastretval ]]; then
-	echo "INFO: inTune password is greater than stored date...using inTune date for reference"
+	echo "INFO: inTune password date is greater than stored date...using inTune date for reference"
+    forceRecon="Yes"
 	retval=$newPasswordDate
 fi
 
+# Store the Password Last Date changed in this file
 if [[ $retval == *"Does Not Exist"* ]]; then
     # Entry does not exist so lets create it and populate the userPassword into it
     retval=$(/usr/libexec/plistbuddy -c "add PasswordLastChanged string $newPasswordDate" $JSS_FILE 2>&1)
     echo "INFO: Created new key 'PasswordLastChanged' with contents $newPasswordDate"
-    [[ ! -z $retval ]] && echo "ERROR: Results of last command: "$retval
-
 else
     #found the key, so let replace (set) it instead
     retval=$(/usr/libexec/plistbuddy -c "set PasswordLastChanged $newPasswordDate" $JSS_FILE 2>&1)	
     echo "INFO: Replaced key 'PasswordLastChanged' with contents $newPasswordDate"
-    [[ ! -z $retval ]] && echo "ERROR: Results of last command: "$retval
 fi
+[[ ! -z $retval ]] && echo "ERROR: Results of last command: "$retval
 
 # Store the Password Age in this file as well
 retval=$(/usr/libexec/plistbuddy -c "print PasswordAge" $JSS_FILE 2>&1)
@@ -334,14 +166,13 @@ if [[ $retval == *"Does Not Exist"* ]]; then
     # Entry does not exist so lets create it and populate the Password Age into it"
     retval=$(/usr/libexec/plistbuddy -c "add PasswordAge string $passwordAge" $JSS_FILE 2>&1)
     echo "INFO: Created new key 'PasswordAge' with contents $passwordAge"
-    [[ ! -z $retval ]] && echo "ERROR: Results of last command: "$retval
 
 else
     #found the key, so let replace (set) it instead
     retval=$(/usr/libexec/plistbuddy -c "set PasswordAge $passwordAge" $JSS_FILE 2>&1)	
     echo "INFO: Replaced key 'PasswordAge' with contents $passwordAge"
-    [[ ! -z $retval ]] && echo "ERROR: Results of last command: "$retval
 fi
+[[ ! -z $retval ]] && echo "ERROR: Results of last command: "$retval
 
 [[ "${forceRecon}" == "Yes" ]] && jamf recon
 exit 0
