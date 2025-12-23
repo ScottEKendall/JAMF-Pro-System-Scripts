d5ec31861163aaf8ee36c118ef9048ff710d9017
Scott Kendall <106453475+ScottEKendall@users.noreply.github.com>
2025-12-23 15:40:15 -0500

    System Scripts that I use for our JAMF server

--- Maintenance - InTune - Groups.sh
+++ Maintenance - InTune - Groups.sh
@@ -3,15 +3,16 @@
 # by: Scott Kendall
 #
 # Written: 09/09/2025
-# Last updated: 09/09/2025
+# Last updated: 12/23/2025
 
 # Script to retrieve users groups from EntraID and store them in the Users plist file
 # 
 # 1.0 - Initial code
+# 1.1 - Add in functions to write out SMB server paths so it can be read in later when using the "Connect Network Drive" script
 #
 ######################################################################################################
 #
-# Gobal "Common" variables
+# Global "Common" variables
 #
 ######################################################################################################
 
@@ -117,6 +118,75 @@ function check_logged_in_user ()
     fi
 }
 
+function create_group_list ()
+{
+    # Determine if users have RO or RW in their group names.  This denotes legacy drive access
+
+    for item in ${MSGRAPH_GROUPS[@]}; do
+        if [[ "${item:u}" == *"_RW"* ]] || [[ "{$item:u}" == *"_RO"* ]] || [[ "{$item:u}" == *"CLIENT TECHNOLOGIES"* ]]; then
+            localGroups+=${item:u}
+        fi
+    done
+}
+
+function modify_group_entries ()
+{
+    # Here is where we make the changes to the users plist file to show the Group Names and drive mappings (SMB path)
+    # Create or modify the Plist entries for EntraGroups if they don't exist
+    retval=$(/usr/libexec/plistbuddy -c "print EntraGroups" $JSS_FILE 2>&1)
+    if [[ "$retval" == *"Does Not Exist"* ]]; then
+        echo "INFO: Creating Entra Groups"
+    else
+        echo "INFO: Updating existing Entra Groups"
+        retval=$(/usr/libexec/PlistBuddy -c "Delete EntraGroups" $JSS_FILE 2>&1)
+        [[ ! -z $retval ]] && {echo "ERROR: Problems deleting array: "$retval; exit 1;} 
+    fi
+
+    retval=$(/usr/libexec/plistbuddy -c "add EntraGroups array" $JSS_FILE 2>&1)
+    # Make sure nothing went wrong while creating the array
+    [[ ! -z $retval ]] && {echo "ERROR: Results of last command: "$retval; exit 1;} 
+
+    for item in ${localGroups[@]}; do
+        echo "INFO: Add Group - "$item
+        /usr/libexec/PlistBuddy -c "Add EntraGroups: string $item" $JSS_FILE 2>&1
+    done
+
+}
+
+function modify_drive_mappings ()
+{
+
+    # Create or modify the Plist entries for DriveMappings if they don't exist
+
+    retval=$(/usr/libexec/plistbuddy -c "print DriveMappings" $JSS_FILE 2>&1)
+    if [[ "$retval" == *"Does Not Exist"* ]]; then
+        echo "INFO: Creating Drive Mappings"
+    else
+        echo "INFO: Updating existing Drive Mappings"
+        retval=$(/usr/libexec/PlistBuddy -c "Delete DriveMappings" $JSS_FILE 2>&1)
+        [[ ! -z $retval ]] && {echo "ERROR: Problems deleting array: "$retval; exit 1;} 
+    fi
+
+    retval=$(/usr/libexec/plistbuddy -c "add DriveMappings array" $JSS_FILE 2>&1)
+    # Make sure nothing went wrong while creating the array
+    [[ ! -z $retval ]] && {echo "ERROR: Results of last command: "$retval; exit 1;} 
+
+    for item in ${localGroups[@]}; do
+        # Look up the group name in the JSON blob and return the drive mapping
+        mapping=$(echo $DriveMappings | jq '.[] | select(.GroupName == "'$item'") | .SMBShare')
+        if [[ ! -z $mapping ]]; then
+            echo "INFO: Add mapping - "$mapping
+            /usr/libexec/PlistBuddy -c "Add DriveMappings: string $mapping" $JSS_FILE 2>&1
+        fi
+    done
+    # Everyone gets the common share in their mappings:
+
+    mapping=$(echo $DriveMappings | jq '.[] | select(.GroupName == "COMMON") | .SMBShare')
+    echo "INFO: Add mapping - "$mapping
+    /usr/libexec/PlistBuddy -c "Add DriveMappings: string $mapping" $JSS_FILE 2>&1
+
+}
+
 ####################################################################################################
 #
 # Main Script
@@ -127,6 +197,21 @@ declare MS_ACCESS_TOKEN
 declare MS_DOMAIN
 declare MSGRAPH_GROUPS=()
 declare localGroups=()
+declare DriveMappings
+
+# This list is based on Entra Group Names
+# You need to create a JSON record that has the Entra Group Names and the SMB server path
+# GroupName must be the exact name (case sensitive) that is in your Azure/Intune groups 
+# SMBShare is the full UNC path to your network share
+
+DriveMappings='[
+{"GroupName" : "MACDATA_RW",
+"SMBShare" : "smb://<uncpath"},
+{"GroupName" : "COMMON",
+"SMBShare" : "smb://<uncpath>"},
+{"GroupName" : "CLIENT TECHNOLOGIES",
+"SMBShare" : "smb://<uncpath>"}
+]'
 
 check_logged_in_user
 
@@ -139,33 +224,11 @@ msgraph_get_access_token
 msgraph_upn_sanity_check
 msgraph_get_group_data
 
-# Determine if users have RO or RW in their groupnames.  This denotes legacy drive access
-
-for item in ${MSGRAPH_GROUPS[@]}; do
-    if [[ "${item:l}" == *"_rw"* ]] || [[ "{$item:l}" == *"_ro"* ]]; then
-        localGroups+=${item:u}
-    fi
-done
-
 # Write out the info it our plist array
 echo "INFO: Microsoft EntraID: "$MS_USER_NAME
 echo "INFO: Plist file: "$JSS_FILE
 
-retval=$(/usr/libexec/plistbuddy -c "print DriveMappings" $JSS_FILE 2>&1)
-if [[ "$retval" == *"Does Not Exist"* ]]; then
-    echo "INFO: Creating Drive Mapping"
-else
-    echo "INFO: Updating existing info"
-    retval=$(/usr/libexec/PlistBuddy -c "Delete DriveMappings" $JSS_FILE 2>&1)
-	[[ ! -z $retval ]] && {echo "ERROR: Problems deleting array: "$retval; exit 1;} 
-fi
-
-retval=$(/usr/libexec/plistbuddy -c "add DriveMappings array" $JSS_FILE 2>&1)
-# Make sure nothing went wrong while creating the array
-[[ ! -z $retval ]] && {echo "ERROR: Results of last command: "$retval; exit 1;} 
-
-for item in ${localGroups[@]}; do
-	echo "INFO: Adding: "$item
-    /usr/libexec/PlistBuddy -c "Add DriveMappings: string $item" $JSS_FILE 2>&1
-done
+create_group_list
+modify_group_entries
+modify_drive_mappings
 exit 0
